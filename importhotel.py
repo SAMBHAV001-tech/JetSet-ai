ced238ad1290e3f341f772038d4143fe7d31a984
metadroix35 <satapathysoumyajeet@gmail.com>
2025-02-02 22:09:11 +0530

    Add files via upload

---
+++ importhotel.py
@@ -0,0 +1,2 @@
+import os
+print(os.path.exists("hotels.json"))  # Should return True
---
+++ rag_model.py
@@ -0,0 +1,8 @@
+from langchain.llms import OpenAI
+from langchain.chains import RetrievalQA
+from langchain.vectorstores import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+print("LangChain imports loaded successfully!")
